503221806a05f529e659a19eb904bd4d8d04231b
Rahul kumar maury <rahu7827759@gmail.com>
2024-02-02 16:32:52 +0530

    Integrate Google Map set option for different view of google map

--- app/src/main/java/com/programmingz/googlemap/MainActivity.kt
+++ app/src/main/java/com/programmingz/googlemap/MainActivity.kt
@@ -2,6 +2,8 @@ package com.programmingz.googlemap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -15,6 +17,26 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val mapOptionButton: ImageButton = findViewById(R.id.mapOptionMenu)
+        val popupMenu = PopupMenu(this, mapOptionButton)
+        popupMenu.menuInflater.inflate(R.menu.map_option, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener {menuItem ->
+            changeMap(menuItem.itemId)
+            true
+        }
+        mapOptionButton.setOnClickListener {
+            popupMenu.show()
+        }
+    }
+
+    private fun changeMap(itemId: Int) {
+        when(itemId){
+            R.id.normal_map -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_NORMAL
+            R.id.hybrid_map -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_HYBRID
+            R.id.terrain_map -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            R.id.satellite_map -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
